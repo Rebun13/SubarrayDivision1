266dab383a817255ada9908d604018a4912d3a2f
Rebun13 <rubengdopazo@gmail.com>
2023-08-04 16:03:22 +0100

    Added the code

---
+++ SubarrayDivision1.cpp
@@ -0,0 +1,108 @@
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+int sum(vector<int> arr, int currentSum, int target, int remaining);
+
+/*
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ */
+
+int birthday(vector<int> s, int d, int m) {
+    int sumUp = 0;
+    for(int i = 0; i < s.size() - m + 1; i++) {
+        int sum = accumulate(s.begin() + i, s.begin() + i + m, 0);
+        if(sum == d) sumUp++;
+        cout << "Sum: " << sum << endl;
+    }
+    return sumUp;
+}
+
+int main()
+{
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string s_temp_temp;
+    getline(cin, s_temp_temp);
+
+    vector<string> s_temp = split(rtrim(s_temp_temp));
+
+    vector<int> s(n);
+
+    for (int i = 0; i < n; i++) {
+        int s_item = stoi(s_temp[i]);
+
+        s[i] = s_item;
+    }
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int d = stoi(first_multiple_input[0]);
+
+    int m = stoi(first_multiple_input[1]);
+
+    int result = birthday(s, d, m);
+
+    cout << result << "\n";
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](int c) {return !std::isspace(c);})
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](int c) {return !std::isspace(c);}).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
